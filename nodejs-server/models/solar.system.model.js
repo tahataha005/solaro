9e861d00623b05b32d0543e509b68223bd3135f9
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 09:16:03 +0300

    Backend: Creating a solar system schema in solar.system.model.js

--- nodejs-server/models/solar.system.model.js
+++ nodejs-server/models/solar.system.model.js
@@ -1,1 +1,20 @@
 const mongoose = require("mongoose");
+
+const solarSystemSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+    connection: {
+        type: String,
+        required: "connection is required",
+    },
+    charging: {
+        type: Number,
+        default: 0,
+    },
+    consumption: {
+        type: Number,
+        default: 0,
+    },
+});
