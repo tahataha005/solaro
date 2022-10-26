1c06e1b390936c054836e8a946bc374297144dc2
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 09:42:15 +0300

    Backend: Creating a peak schema peak.model.js

--- nodejs-server/models/peak.model.js
+++ nodejs-server/models/peak.model.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+//Creating peak schema
+const peakSchema = mongoose.Schema({
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+    },
+    peak: {
+        type: Number,
+        required: "peak is required",
+    },
+    timestamp: {
+        type: date,
+        required: "date is required",
+    },
+});
