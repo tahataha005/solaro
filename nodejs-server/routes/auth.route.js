14499bfa49bb41d08016d51738603dde2c0701b5
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 14:09:34 +0300

    Backend: Assigning a route for for signUp function in auth.route.js

--- nodejs-server/routes/auth.route.js
+++ nodejs-server/routes/auth.route.js
@@ -2,4 +2,7 @@ const Router = require("express");
 
 const router = Router();
 
+const { signup } = require("../controllers/auth.controller.js");
+router.post("/", signup);
+
 module.exports = router;
