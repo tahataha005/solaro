5fcadb6f16d5ca012cc10b5cc9ce2f95b0ff7d6b
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 22:01:11 +0300

    Backend: Creating getAllItems function in read.controller.js

--- nodejs-server/controllers/read.controller.js
+++ nodejs-server/controllers/read.controller.js
@@ -1,6 +1,7 @@
 const SolarSystem = require("../models/solar.system.model.js");
 const Item = require("../models/item.model.js");
 
+//Searching for system by user id
 const getSolarStats = async (req, res) => {
     try {
         //Destructuring req data
@@ -20,4 +21,26 @@ const getSolarStats = async (req, res) => {
     }
 };
 
-module.exports = { getSolarStats };
+//Searching for all items belonging to solar system by system id
+const getAllItems = async (req, res) => {
+    try {
+        //Destructuring req data
+        const { system_id } = req.params;
+
+        //Geting items belonging to system by its id
+        const items = await Item.find({ solar_system: system_id });
+
+        //If not found return not found
+        if (!items)
+            return res
+                .status(404)
+                .json({ message: "No items found or system id doesn't exist" });
+
+        //Returning recieved items
+        res.status(200).json(items);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = { getSolarStats, getAllItems };
