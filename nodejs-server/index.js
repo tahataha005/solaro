f7f43e84548effda02c7a42a7f6770960003c1a1
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 14:22:35 +0300

    Backend: Importing and using auth routes

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -4,9 +4,14 @@ require("./config/db.config.js");
 
 //Initilizing app as express method
 const app = express();
+app.use(express.json());
 
 //Listening on server port and logging status
 app.listen(process.env.PORT, err => {
     if (err) console.log(err);
     else console.log(`Server is running on PORT ${process.env.PORT}`);
 });
+
+//Using created routes
+const authRoutes = require("./routes/auth.route.js");
+app.use("/auth", authRoutes);
