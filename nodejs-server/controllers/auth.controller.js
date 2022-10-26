1e24f87388a24bef9b02e68dede312e9a569d53b
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 14:07:40 +0300

    Backend: Creating signUp function in auth.controller.js

--- nodejs-server/controllers/auth.controller.js
+++ nodejs-server/controllers/auth.controller.js
@@ -1,1 +1,20 @@
 const User = require("../models/user.model.js");
+const bcrypt = require("bcrypt");
+
+const signUp = async (req, res) => {
+    const { user_type, email, password } = req.body;
+    const user = new User();
+
+    try {
+        user.user_type = user_type;
+        user.email = email;
+        user.password = await bcrypt.hash(password, 10);
+
+        await user.save();
+        res.json(user);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = { signup };
