c95a7e5ba3c3c8ab884dc7b340d7a65fcb0fda96
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 07:43:11 +0300

    Backend: Requiring database configuration in index.js

--- nodejs-server/config/db.config.js
+++ nodejs-server/config/db.config.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
 
 mongoose
-    .connect("localhost:27017/solarodb")
-    .then(
-        console
-            .log("Connected to database")
-            .catch(err => console.log(`database err ${err}`))
-    );
+    .connect("mongodb://localhost:27017/solarodb")
+    .then(console.log("Connected to database"))
+    .catch(err => console.log(`database err ${err}`));
--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 require("dotenv").config();
+require("./config/db.config.js");
 
 //Initilizing app as express method
 const app = express();
