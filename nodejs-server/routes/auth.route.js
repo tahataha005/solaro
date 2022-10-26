0ccffa43e4720c831b8491635d6a9ac0040b5e71
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 14:20:55 +0300

    Backend: Commenting sign up route in auth.route.js

--- nodejs-server/routes/auth.route.js
+++ nodejs-server/routes/auth.route.js
@@ -1,8 +1,12 @@
 const Router = require("express");
 
+//Creating a router
 const router = Router();
 
+//Importing functions from controllers
 const { signup } = require("../controllers/auth.controller.js");
+
+//Creating routes
 router.post("/", signup);
 
 module.exports = router;
