3f32c4e0bb743a2c4e6f44c245e53a40258d5bda
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 14:47:39 +0300

    Backend: Creating logIn function in auth.controller.js

--- nodejs-server/controllers/auth.controller.js
+++ nodejs-server/controllers/auth.controller.js
@@ -22,4 +22,26 @@ const signUp = async (req, res) => {
     }
 };
 
-module.exports = { signUp };
+//Authenticating a user
+const logIn = async (req, res) => {
+    //Destructuring req data
+    const { email, password } = req.body;
+
+    //Searching for given email
+    const user = await User.findOne({ email });
+
+    //If email not in found return invalid
+    if (!user) return res.status(404).json({ message: "Invalid Credentials" });
+
+    //Comparing given pass with hashed user pass
+    const passCheck = await bcrypt.compare(password, user.password);
+
+    //If not comparison returned false
+    if (!passCheck)
+        return res.status(404).json({ message: "Invalid Credentials" });
+
+    //Return authenticated
+    res.status(200).json({ message: "Authenticated" });
+};
+
+module.exports = { signUp, logIn };
