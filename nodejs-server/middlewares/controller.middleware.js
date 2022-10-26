9bfd9fe269e2f576ff9cbd9b6e0d369c3cff4fe2
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 18:02:46 +0300

    Backend: Creating the controllerMiddleware function to check if the user is a controller in controller.middleware.js

--- nodejs-server/middlewares/controller.middleware.js
+++ nodejs-server/middlewares/controller.middleware.js
@@ -1,3 +1,14 @@
-const controllerMiddleware = (erq, res, next) => {};
+const controllerMiddleware = (req, res, next) => {
+    //Checcking if user type is controller
+    try {
+        //If true => pass
+        if (req.user.user_type.user_type === "controller") next();
+        
+        //If false => unauthorized
+        else res.status(401).json({ message: "Unauthorized" });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
 
 module.exports = controllerMiddleware;
