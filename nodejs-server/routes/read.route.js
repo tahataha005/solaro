08182263175e7815d28d2b79235f486a6dd6d390
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 21:01:20 +0300

    Backend: Assigning a route for getSolarStats function in read.route.js

--- nodejs-server/routes/read.route.js
+++ nodejs-server/routes/read.route.js
@@ -1,3 +1,9 @@
 const { Router } = require("express");
 
 const router = Router();
+
+const { getSolarStats } = require("../controllers/read.controller.js");
+
+router.get("/system/:user_id", getSolarStats);
+
+module.exports = router;
