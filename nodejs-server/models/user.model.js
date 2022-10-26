d8cc96eff73e3f07e37ae1301d71052e57c73d72
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 08:01:59 +0300

    Backend: Creating a user schema in user.model.js

--- nodejs-server/models/user.model.js
+++ nodejs-server/models/user.model.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+
+//Creating a user schema
+const userSchema = mongoose.Schema({
+    email: {
+        type: String,
+        required: "email is required",
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: "password is required",
+        trim: true,
+    },
+});
