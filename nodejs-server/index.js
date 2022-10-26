b5941eb33a4934f9bf6fad04959da64f62c3e4be
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 07:22:45 +0300

    Backend: Using express framework and listening on server port in index.js

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,1 +1,10 @@
-//Server
+const express = require("express");
+
+//Initilizing app as express method
+const app = express();
+
+//Listening on server port and logging status
+app.listen(8000, err => {
+    if (err) console.log(err);
+    else console.log("Server is running on PORT 8000");
+});
