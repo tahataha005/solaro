e8976961b4277aee00f71e5e18e46703324a8eba
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 08:21:19 +0300

    Backend: Creating user type schema in user.type.model.js

--- nodejs-server/models/user.type.model.js
+++ nodejs-server/models/user.type.model.js
@@ -1,1 +1,11 @@
 const mongoose = require("mongoose");
+
+//Creating user type schema
+const userTypeSchema = mongoose.Schema({
+    user_type: {
+        type: String,
+        requried: true,
+        unique: true,
+        trim: true,
+    },
+});
