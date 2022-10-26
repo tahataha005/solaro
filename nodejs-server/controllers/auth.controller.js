dbb213531a08bcd9cbdf043cca2e13d89e3b2170
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 14:20:21 +0300

    Backend: Commenting signUp function

--- nodejs-server/controllers/auth.controller.js
+++ nodejs-server/controllers/auth.controller.js
@@ -1,15 +1,20 @@
 const User = require("../models/user.model.js");
 const bcrypt = require("bcrypt");
 
+//Function to sign up a new user
 const signUp = async (req, res) => {
+    //Destructuring req data
     const { user_type, email, password } = req.body;
-    const user = new User();
 
     try {
+        const user = new User();
+
+        //Assigning user credentials
         user.user_type = user_type;
         user.email = email;
         user.password = await bcrypt.hash(password, 10);
 
+        //Saving the created user in database
         await user.save();
         res.json(user);
     } catch (err) {
@@ -17,4 +22,4 @@ const signUp = async (req, res) => {
     }
 };
 
-module.exports = { signup };
+module.exports = { signUp };
