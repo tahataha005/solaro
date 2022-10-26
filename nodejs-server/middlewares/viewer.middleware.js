4d063b5cca7a2aeb2098e956897057626cc8e01a
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 18:24:16 +0300

    Backend: Creating the viewerMiddleware function to check if the user is a viewer in viewer.middleware.js

--- nodejs-server/middlewares/viewer.middleware.js
+++ nodejs-server/middlewares/viewer.middleware.js
@@ -1,3 +1,13 @@
-const viewerMiddleware = (req, res, next) => {};
+const viewerMiddleware = (req, res, next) => {
+    //Checcking if user type is viewer
+    try {
+        //If true => pass
+        if (req.user.user_type.user_type === "viewer") next();
+        //If false => unauthorized
+        else res.status(401).json({ message: "Unauthorized" });
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
 
 module.exports = viewerMiddleware;
