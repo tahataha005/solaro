5c1836b4d4a22ed16ae945a1bbfefb54c9e90d3d
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 09:29:33 +0300

    Backend: Creating an item schema in item.model.js

--- nodejs-server/models/item.model.js
+++ nodejs-server/models/item.model.js
@@ -1,1 +1,26 @@
 const mongoose = require("mongoose");
+
+//Creating an item schema
+const itemSchema = mongoose.Schema({
+    solar_system: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "SolarSystem",
+    },
+    name: {
+        type: String,
+        required: "name is required",
+        trim: true,
+    },
+    ideal_consumption: {
+        type: Number,
+        required: "ideal consumption is required",
+    },
+    live_consumption: {
+        type: Number,
+        default: 0,
+    },
+    timestamp: {
+        type: Date,
+        default: Date.now,
+    },
+});
