9ba78cdd11a091ea7808a167e00935c09839a489
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 20:07:51 +0300

    Backend: Creating getSolarStats function to get system by user id in read.controller.js

--- nodejs-server/controllers/read.controller.js
+++ nodejs-server/controllers/read.controller.js
@@ -1,2 +1,23 @@
 const SolarSystem = require("../models/solar.system.model.js");
 const Item = require("../models/item.model.js");
+
+const getSolarStats = async (req, res) => {
+    try {
+        //Destructuring req data
+        const { user_id } = req.params;
+
+        //Geting system belonging to user by his id
+        const system = await SolarSystem.findOne({ user: user_id });
+
+        //If not found return not found
+        if (!system)
+            return res.status(404).json({ message: "Solar System not found" });
+
+        //Returning recieved system
+        res.status(200).json(system);
+    } catch (err) {
+        res.status(400).json({ message: err.message });
+    }
+};
+
+module.exports = { getSolarStats };
