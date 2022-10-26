3f7b3d48b62efea74562470c38603ae1c51a0f22
tahataha005 <taha.f.taha0@gmail.com>
2022-10-26 09:37:27 +0300

    Backend: Creating history schema in history.model.js

--- nodejs-server/models/history.model.js
+++ nodejs-server/models/history.model.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose");
+
+//Creating history schema
+const historySchema = mongoose.Schema({
+    item: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+    },
+    start: {
+        type: date,
+        required: "start date is required",
+    },
+    end: {
+        type: date,
+        required: "end date is required",
+    },
+    average_consumtion: {
+        type: Number,
+        required: "average consumption is required",
+    },
+});
