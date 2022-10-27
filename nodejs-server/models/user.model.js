ff2b7cd070e0f3b5fca90c4833fa26536a5a9b3d
tahataha005 <taha.f.taha0@gmail.com>
2022-10-27 20:38:33 +0300

    Backend: Embeding solar system schema in user schema in user.model.js

--- nodejs-server/models/user.model.js
+++ nodejs-server/models/user.model.js
@@ -1,22 +1,40 @@
 const mongoose = require("mongoose");
 
+//Creating a solar system schema
+const solarSystemSchema = mongoose.Schema({
+    connection: {
+        type: String,
+        required: "connection is required",
+    },
+    charging: {
+        type: Number,
+        default: 0,
+    },
+    consumption: {
+        type: Number,
+        default: 0,
+    },
+});
+
 //Creating a user schema
 const userSchema = mongoose.Schema({
-    user_type: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "UserType",
-    },
     email: {
         type: String,
         required: "email is required",
         unique: true,
         trim: true,
     },
+
     password: {
         type: String,
         required: "password is required",
         trim: true,
     },
+    user_type: {
+        type: String,
+        required: true,
+    },
+    system: [solarSystemSchema],
 });
 
 //Creating a user model using user schema
