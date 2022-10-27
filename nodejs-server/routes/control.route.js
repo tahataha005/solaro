47a9165e73c506db523a50c4da71e8c9551ce7b7
tahataha005 <taha.f.taha0@gmail.com>
2022-10-27 11:45:59 +0300

    Backend: Creating control.route.js

--- nodejs-server/routes/control.route.js
+++ nodejs-server/routes/control.route.js
@@ -1,5 +1,12 @@
 const { Router } = require("express");
 
+//Creating a router
 const router = Router();
+
+//Importing functions from controllers
+const { addSolarSystem } = require("../controllers/control.controller.js");
+
+//Creating routes
+router.post("/system", addSolarSystem);
 
 module.exports = router;
