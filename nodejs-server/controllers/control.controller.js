cb4a8dd9645a5d7c9a801219d3eec94b0ed152b0
tahataha005 <taha.f.taha0@gmail.com>
2022-10-28 15:17:52 +0300

    Backend: Editing functions in control.controller.js after embeding items in solar schema

--- nodejs-server/controllers/control.controller.js
+++ nodejs-server/controllers/control.controller.js
@@ -30,27 +30,37 @@ const addSolarSystem = async (req, res) => {
     }
 };
 
+//Adding w new item to solar system
 const addItem = async (req, res) => {
     //Destructuring req data
-    const { solar_system, name, ideal_consumption } = req.body;
+    const { user_id, system_name, name, ideal_consumption } = req.body;
 
     try {
-        //Creating a new instance of Item model
-        const item = new Item();
+        //Getting user by id
+        const user = await User.findById(user_id);
 
-        //Assigning item attributes
-        item.solar_system = solar_system;
-        item.name = name;
-        item.ideal_consumption = ideal_consumption;
-        item.status = false;
-        item.live_consumption = 0;
-        item.timestamp = null;
+        //Searching for solar system to add item to
+        const system = user.system.filter(system => {
+            return system.name == system_name;
+        })[0];
 
-        //Saving item in database
-        await item.save();
+        //Creating and new item object
+        const item = {
+            name: name,
+            ideal_consumption: ideal_consumption,
+            status: false,
+            live_consumption: 0,
+            timestamp: null,
+        };
 
-        //Returning created item id
-        res.status(200).json({ item_id: item._id });
+        //Adding item to solar system
+        system.items.push(item);
+
+        //Saving user
+        await user.save();
+
+        //Returning created item
+        res.status(200).json({ added: item });
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
@@ -80,58 +90,103 @@ const dropSolarSystem = async (req, res) => {
     }
 };
 
-//Deleting item by id
+//Deleting item by name
 const dropItem = async (req, res) => {
-    //Destructuring req data
-    const { item_id } = req.body;
+    const { user_id, system_name, item_name } = req.body;
 
     try {
-        //Searching for item by id and deleting it
-        const item = await Item.findByIdAndDelete(item_id);
+        //Getting user by id to delete solar system from
+        const user = await User.findById(user_id);
+
+        //Filtering array of solar systems
+        const system = user.system.filter(system => {
+            return system.name == system_name;
+        })[0];
+
+        //Filtering items to remove desired item
+        system.items = system.items.filter(item => {
+            return item.name != item_name;
+        });
+
+        //Saving changes in user
+        await user.save();
 
         //Returning deleted item
-        res.status(200).json({ deleted: item });
+        res.status(200).json(user.system.items);
     } catch (err) {
         res.status(200).json({ message: err.message });
     }
 };
 
-//Editing item by its id
+//Editing item by name
 const editItem = async (req, res) => {
     //Destructuring req data
-    const { item_id, name, ideal_consumption } = req.body;
+    const { user_id, system_name, item_name, name, ideal_consumption } =
+        req.body;
 
     try {
-        //Updating item by it
-        const item = await Item.findByIdAndUpdate(item_id, {
-            name: name,
-            ideal_consumption: ideal_consumption,
-        });
+        //Getting user by id
+        const user = await User.findById(user_id);
+
+        //Filtering array of solar systems
+        const system = user.system.filter(system => {
+            return system.name == system_name;
+        })[0];
+
+        //Filtering items to get desired item
+        const item = system.items.filter(item => {
+            return item.name == item_name;
+        })[0];
+
+        //Updating item according to obtained attributes
+        name ? (item.name = name) : (item.name = item.name);
+        ideal_consumption
+            ? (item.ideal_consumption = ideal_consumption)
+            : (item.ideal_consumption = item.ideal_consumption);
+
+        //Saving changes in user
+        await user.save();
 
         //Returning updated item
-        res.status(200).json({ updated: item });
+        res.status(200).json(item);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
 };
 
 const controlItem = async (req, res) => {
-    const { item_id, status } = req.body;
+    //Destructuring req data
+    const { user_id, system_name, item_name, trigger } = req.body;
+
+    //Assigning status according to request
+    let status = true;
+    trigger == "off" ? (status = false) : (status = status);
 
     try {
-        if (status === "on") {
-            const item = await Item.findByIdAndUpdate(item_id, {
-                status: true,
-            });
-            return res.status(200).json({ message: `${item.name} turned on` });
-        }
-        if (status === "off") {
-            const item = await Item.findByIdAndUpdate(item_id, {
-                status: false,
-            });
-            return res.status(200).json({ message: `${item.name} turned off` });
-        }
-        res.status(400).json({ message: "a problem occured assigning status" });
+        //Getting user by id
+        const user = await User.findById(user_id);
+
+        //Filtering array of solar systems
+        const system = user.system.filter(system => {
+            return system.name == system_name;
+        })[0];
+
+        //Filtering items to get desired item
+        const item = system.items.filter(item => {
+            return item.name == item_name;
+        })[0];
+
+        //if item not found return not found
+        if (!item) return res.status(404).json({ message: "Item not found" });
+
+        //Updating item according to obtained attributes
+        item.status = status;
+
+        //Saving changes in user
+        await user.save();
+
+        //Returning updated item
+        res.status(200).json(item);
     } catch (err) {
         res.status(400).json({ message: err.message });
     }
