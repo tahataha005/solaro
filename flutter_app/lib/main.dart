0f77c325194e2c95f54000c02eb1648bfa1c6eff
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 16:46:06 +0200

    Frontend: Adding font themes in main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -12,6 +12,35 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: createMaterialColor(Color.fromRGBO(0, 114, 187, 1)),
         accentColor: Color.fromRGBO(255, 186, 73, 1),
+        fontFamily: "Inter",
+        textTheme: ThemeData.light().textTheme.copyWith(
+              labelMedium: const TextStyle(
+                fontFamily: "Inter",
+                fontSize: 20,
+                color: Colors.white,
+              ),
+              labelLarge: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white),
+              titleMedium: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold),
+              bodySmall: const TextStyle(
+                fontFamily: "Inter",
+                fontSize: 16,
+              ),
+              bodyMedium: const TextStyle(
+                fontFamily: "Inter",
+                fontSize: 20,
+              ),
+              bodyLarge: const TextStyle(
+                fontFamily: "Inter",
+                fontSize: 24,
+              ),
+            ),
       ),
       home: RegisterPage(),
     );
