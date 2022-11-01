d8867941113b18b86e8b12682fb0b78156299310
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 18:10:53 +0200

    Frontend: Adding appBar elements of landing page

--- flutter_app/lib/pages/landing.page.dart
+++ flutter_app/lib/pages/landing.page.dart
@@ -8,6 +8,22 @@ class LandingPage extends StatefulWidget {
 class _LandingPageState extends State<LandingPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Image.asset(
+          "assets/images/Header-Logo.png",
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.notifications_active_outlined,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
