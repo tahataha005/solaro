c0933e5392d50761990604ef59cc2e9442a31652
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 16:51:55 +0200

    Frontend: Wrapping registration page with scroll view to addapt with keyboard

--- flutter_app/lib/pages/register/register.page.dart
+++ flutter_app/lib/pages/register/register.page.dart
@@ -13,44 +13,47 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-      padding: EdgeInsets.symmetric(
-        horizontal: MediaQuery.of(context).size.width * 0.1,
-        // vertical: MediaQuery.of(context).size.width * 0.2,
-      ),
-      child: Column(
-        children: [
-          Container(
-            width: MediaQuery.of(context).size.width * 0.4,
-            margin: EdgeInsets.symmetric(
-                vertical: MediaQuery.of(context).size.width * 0.2),
-            child: Image.asset(
-              "assets/images/Logo.png",
-              fit: BoxFit.cover,
-            ),
-          ),
-          content ? Login() : SignUp(),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                content ? "Don't have an account?" : "Already have an account?",
-                style: Theme.of(context).textTheme.bodySmall,
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.symmetric(
+          horizontal: MediaQuery.of(context).size.width * 0.1,
+        ),
+        child: Column(
+          children: [
+            Container(
+              width: MediaQuery.of(context).size.width * 0.4,
+              margin: EdgeInsets.symmetric(
+                  vertical: MediaQuery.of(context).size.width * 0.2),
+              child: Image.asset(
+                "assets/images/Logo.png",
+                fit: BoxFit.cover,
               ),
-              TextButton(
-                onPressed: () {
-                  setState(() {
-                    content = !content;
-                  });
-                },
-                child: Text(
-                  content ? "Sign Up" : "Log In",
-                  style: TextStyle(fontFamily: "Inter", fontSize: 16),
+            ),
+            content ? Login() : SignUp(),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  content
+                      ? "Don't have an account?"
+                      : "Already have an account?",
+                  style: Theme.of(context).textTheme.bodySmall,
                 ),
-              )
-            ],
-          )
-        ],
+                TextButton(
+                  onPressed: () {
+                    setState(() {
+                      content = !content;
+                    });
+                  },
+                  child: Text(
+                    content ? "Sign Up" : "Log In",
+                    style: TextStyle(fontFamily: "Inter", fontSize: 16),
+                  ),
+                )
+              ],
+            )
+          ],
+        ),
       ),
     ));
   }
