9a4b049fb813dbb129ccd529734cc0bce996080d
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 16:45:27 +0200

    Frontend: Switching login and signup content on bottom button click in register page

--- flutter_app/lib/pages/register/register.page.dart
+++ flutter_app/lib/pages/register/register.page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import './login.content.dart';
 import './signup.content.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -9,24 +8,48 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  bool content = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         body: Padding(
       padding: EdgeInsets.symmetric(
         horizontal: MediaQuery.of(context).size.width * 0.1,
-        vertical: MediaQuery.of(context).size.width * 0.15,
+        // vertical: MediaQuery.of(context).size.width * 0.2,
       ),
       child: Column(
         children: [
           Container(
-            width: MediaQuery.of(context).size.width * 0.36,
+            width: MediaQuery.of(context).size.width * 0.4,
+            margin: EdgeInsets.symmetric(
+                vertical: MediaQuery.of(context).size.width * 0.2),
             child: Image.asset(
               "assets/images/Logo.png",
               fit: BoxFit.cover,
             ),
           ),
-          /*Login()*/ SignUp(),
+          content ? Login() : SignUp(),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                content ? "Don't have an account?" : "Already have an account?",
+                style: Theme.of(context).textTheme.bodySmall,
+              ),
+              TextButton(
+                onPressed: () {
+                  setState(() {
+                    content = !content;
+                  });
+                },
+                child: Text(
+                  content ? "Sign Up" : "Log In",
+                  style: TextStyle(fontFamily: "Inter", fontSize: 16),
+                ),
+              )
+            ],
+          )
         ],
       ),
     ));
