3a576835301f61dd9484fa2e6c5019f71b9a6997
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 12:31:23 +0200

    Frontend: Creating register folder in pages for registration page content

---
+++ flutter_app/lib/pages/register/register.page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  State<RegisterPage> createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+      children: [
+        Image.asset(""), /*Login(), SignUp*/
+      ],
+    ));
+  }
+}
