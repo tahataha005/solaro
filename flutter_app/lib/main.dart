58a25a8e9d676af433350f4ce3ae7e45c9d98be6
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 12:21:39 +0200

    Frontend: Returning material app to MyApp class in main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_app/pages/register.page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: RegisterPage(),
+    );
   }
 }
