a0b9e50ca1c813c1e0130973a28eae9f65c47963
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 18:11:29 +0200

    Frontend: Stying appBar  of landing page

--- flutter_app/lib/pages/landing.page.dart
+++ flutter_app/lib/pages/landing.page.dart
@@ -10,18 +10,28 @@ class _LandingPageState extends State<LandingPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Image.asset(
-          "assets/images/Header-Logo.png",
+        toolbarHeight: 100,
+        title: Container(
+          width: 180,
+          child: Image.asset(
+            "assets/images/Header-Logo.png",
+            fit: BoxFit.cover,
+          ),
         ),
         backgroundColor: Colors.white,
         elevation: 0,
         actions: [
-          IconButton(
-            onPressed: () {},
-            icon: Icon(
-              Icons.notifications_active_outlined,
+          SizedBox(
+            width: 100,
+            child: IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.notifications_active_outlined,
+                color: Theme.of(context).primaryColor,
+                size: 30,
+              ),
             ),
-          ),
+          )
         ],
       ),
     );
