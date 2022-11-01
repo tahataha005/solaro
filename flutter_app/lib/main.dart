eaa7885cfaeeaf5dbbe7dfeda8a9a3b7c5eda52b
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 14:44:13 +0200

    Frontend: Creating tools containing color constructor

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -1,6 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/pages/register.page.dart';
+import 'package:flutter_app/pages/register/register.page.dart';
 
 void main() => runApp(MyApp());
 
@@ -8,6 +10,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Color.fromRGBO(0, 114, 187, 1),
+        accentColor: Color.fromRGBO(255, 186, 73, 1),
+      ),
       home: RegisterPage(),
     );
   }
