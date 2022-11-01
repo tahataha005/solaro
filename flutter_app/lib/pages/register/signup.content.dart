ff45984e29aca3edbfa0cb13032733a983b38105
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 16:44:30 +0200

    Frontend: Styling signup content

--- flutter_app/lib/pages/register/signup.content.dart
+++ flutter_app/lib/pages/register/signup.content.dart
@@ -9,16 +9,51 @@ class _SignUpState extends State<SignUp> {
   @override
   Widget build(BuildContext context) {
     return Container(
+      // color: Colors.amber,
       height: MediaQuery.of(context).size.height * 0.5,
       child: Column(
         children: [
-          TextField(),
-          TextField(),
-          TextField(),
-          ElevatedButton(
-            onPressed: () {},
-            child: Text(
-              "SIGN UP",
+          TextField(
+            decoration: InputDecoration(
+              label: Text(
+                "Email",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: Text(
+                "Password",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 35,
+          ),
+          TextField(
+            decoration: InputDecoration(
+              label: Text(
+                "Confirm Password",
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 70,
+          ),
+          SizedBox(
+            width: double.infinity,
+            height: 60,
+            child: ElevatedButton(
+              onPressed: () {},
+              child: Text(
+                "SIGN UP",
+              ),
             ),
           ),
         ],
