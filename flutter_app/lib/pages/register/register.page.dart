efbd3fec3b335a47b47c3917eec6a563e5482c56
tahataha005 <taha.f.taha0@gmail.com>
2022-11-01 13:06:07 +0200

    Frontend: Importing logo in register.page.dart

--- flutter_app/lib/pages/register/register.page.dart
+++ flutter_app/lib/pages/register/register.page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import './signup.content.dart';
 
 class RegisterPage extends StatefulWidget {
   @override
@@ -11,10 +12,23 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Column(
-      children: [
-        Image.asset(""), /*Login(), SignUp*/
-      ],
+        body: Padding(
+      padding: EdgeInsets.symmetric(
+        horizontal: MediaQuery.of(context).size.width * 0.1,
+        vertical: MediaQuery.of(context).size.width * 0.15,
+      ),
+      child: Column(
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width * 0.36,
+            child: Image.asset(
+              "assets/images/Logo.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+          /*Login()*/ SignUp(),
+        ],
+      ),
     ));
   }
 }
