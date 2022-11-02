05576825fc279a0c3e51bfa57a76427042c160b7
tahataha005 <taha.f.taha0@gmail.com>
2022-11-02 08:04:01 +0200

    Frontend: Creating notifications.page.dart

---
+++ flutter_app/lib/pages/notifications.page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class NotificationsPage extends StatefulWidget {
+  @override
+  State<NotificationsPage> createState() => _NotificationsPageState();
+}
+
+class _NotificationsPageState extends State<NotificationsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
