0029c8fc087b3f197eaa742091bfea1be3506833
tahataha005 <taha.f.taha0@gmail.com>
2022-11-02 13:45:48 +0200

    Frontend: Adding consumption section to main.page.dart

--- flutter_app/lib/pages/main.page.dart
+++ flutter_app/lib/pages/main.page.dart
@@ -8,6 +8,58 @@ class MainPage extends StatefulWidget {
 class _MainPageState extends State<MainPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 100,
+        title: Center(
+          child: Container(
+            width: 180,
+            child: Image.asset(
+              "assets/images/Header-Logo.png",
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          color: Theme.of(context).primaryColorLight,
+        ),
+        child: Column(children: [
+          Container(
+            width: double.infinity,
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              color: Colors.white,
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  "Consumption:",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Container(
+                  child: Container(),
+                ),
+                Text(
+                  "Consumption:",
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+                Container(
+                  child: Container(),
+                ),
+              ],
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
