2f4c4853a60f72f8a15ed9d013afbc57a4398db5
tahataha005 <taha.f.taha0@gmail.com>
2022-11-02 10:19:42 +0200

    Frontend: Styling leading card box

--- flutter_app/lib/pages/notifications.page.dart
+++ flutter_app/lib/pages/notifications.page.dart
@@ -32,7 +32,41 @@ class _NotificationsPageState extends State<NotificationsPage> {
       ),
       body: Column(
         children: notifications.map((notification) {
-          return Text(notification.system_name);
+          return InkWell(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Color.fromARGB(255, 223, 223, 223)),
+                ),
+              ),
+              height: MediaQuery.of(context).size.height * 0.15,
+              child: Row(children: [
+                Container(
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  child: Center(
+                    child: Text(
+                      notification.system_name[0],
+                      style: TextStyle(
+                        fontFamily: "Kanit",
+                        fontWeight: FontWeight.bold,
+                        fontSize: 50,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ),
+                  height: MediaQuery.of(context).size.height * 0.08,
+                  width: MediaQuery.of(context).size.height * 0.08,
+                  decoration: BoxDecoration(
+                    color: Colors.amber,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(10),
+                    ),
+                  ),
+                ),
+              ]),
+            ),
+          );
         }).toList(),
       ),
     );
