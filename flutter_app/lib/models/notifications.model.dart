809c74e96338f5c99f3a536272fcbd998dc8e4cb
tahataha005 <taha.f.taha0@gmail.com>
2022-11-02 08:23:08 +0200

    Frontend: Assigning properties of notifications.model.dart

--- flutter_app/lib/models/notifications.model.dart
+++ flutter_app/lib/models/notifications.model.dart
@@ -1,1 +1,13 @@
-class Notifications {}
+class Notifications {
+  final String system_name;
+  final String title;
+  final String content;
+  final String time;
+
+  Notifications({
+    required this.system_name,
+    required this.title,
+    required this.content,
+    required this.time,
+  });
+}
