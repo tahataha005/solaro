22ec05f14addbd84b0ed2f890f37c7eaba33bb6a
tahataha005 <taha.f.taha0@gmail.com>
2022-11-02 14:49:39 +0200

    Frontend: Styling charging section elements

--- flutter_app/lib/pages/main.page.dart
+++ flutter_app/lib/pages/main.page.dart
@@ -6,8 +6,14 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+  double maxConsumption = 7;
+  double system_consumption = 3.4;
+  double system_charging = 3.5;
+
   @override
   Widget build(BuildContext context) {
+    double currentConsumption = system_consumption / 7;
+    double currentCharging = system_charging / 7;
     return Scaffold(
       appBar: AppBar(
         toolbarHeight: 100,
@@ -41,18 +47,69 @@ class _MainPageState extends State<MainPage> {
             child:
                 Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
               Text(
-                "Consumption:",
-                style: Theme.of(context).textTheme.titleMedium,
-              ),
-              Container(
-                child: Container(),
-              ),
-              Text(
                 "Charging:",
                 style: Theme.of(context).textTheme.titleMedium,
               ),
-              Container(
-                child: Container(),
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15),
+                    height: 40,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColorLight,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  FractionallySizedBox(
+                    widthFactor: currentCharging,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 15),
+                      height: 40,
+                      decoration: BoxDecoration(
+                          color: Theme.of(context).accentColor,
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  )
+                ],
+              ),
+              Text(
+                "Consumption:",
+                style: Theme.of(context).textTheme.titleMedium,
+              ),
+              Stack(
+                children: [
+                  Container(
+                    margin: EdgeInsets.symmetric(vertical: 15),
+                    height: 40,
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: Theme.of(context).primaryColorLight,
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                  FractionallySizedBox(
+                    widthFactor: currentConsumption,
+                    child: Container(
+                      margin: EdgeInsets.symmetric(vertical: 15),
+                      height: 40,
+                      decoration: BoxDecoration(
+                          // gradient: LinearGradient(
+                          //   begin: Alignment.center,
+                          //   end: Alignment.bottomRight,
+                          //   colors: [
+                          //     Theme.of(context).accentColor,
+                          //     Colors.red,
+                          //   ],
+                          //   stops: [0, currentConsumption - currentCharging],
+                          // ),
+                          color: currentConsumption >= currentCharging
+                              ? Colors.red
+                              : Theme.of(context).accentColor,
+                          borderRadius: BorderRadius.circular(10)),
+                    ),
+                  )
+                ],
               ),
             ]),
           ),
