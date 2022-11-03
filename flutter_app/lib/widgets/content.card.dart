cc66dc5caacda4542f0efb1b8edef0afafef0c6a
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 07:59:52 +0200

    Frontend: Assigning properties and constructor of ContentCard

--- flutter_app/lib/widgets/content.card.dart
+++ flutter_app/lib/widgets/content.card.dart
@@ -2,6 +2,12 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class ContentCard extends StatelessWidget {
+  final Widget content;
+
+  ContentCard({
+    required this.content,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container();
