aef755bf00deb8d119d1db5e3aad2dacf75c0f64
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 12:20:45 +0200

    Frontend: Creating top section of details section

--- flutter_app/lib/pages/details.page.dart
+++ flutter_app/lib/pages/details.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/costumed.button.dart';
 
 class DetailsPage extends StatefulWidget {
   @override
@@ -8,6 +9,64 @@ class DetailsPage extends StatefulWidget {
 class _DetailsPageState extends State<DetailsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    String dropdownvalue = 'Monday';
+
+    var drops = [
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday',
+    ];
+    return Scaffold(
+      body: Container(
+        padding: EdgeInsets.all(20),
+        width: double.infinity,
+        child: Column(
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).padding.top,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                IconButton(
+                  onPressed: () {},
+                  icon: Icon(Icons.arrow_back),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Colors.amber,
+                  ),
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  child: DropdownButton(
+                    style: Theme.of(context).textTheme.bodyMedium,
+                    value: dropdownvalue,
+                    icon: const Icon(Icons.keyboard_arrow_down),
+                    items: drops.map((String items) {
+                      return DropdownMenuItem(
+                        value: items,
+                        child: Text(items),
+                      );
+                    }).toList(),
+                    onChanged: (String? newValue) {
+                      setState(() {
+                        dropdownvalue = newValue!;
+                      });
+                    },
+                  ),
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
