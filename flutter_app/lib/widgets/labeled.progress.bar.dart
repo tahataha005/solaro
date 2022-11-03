e4f3afe5203d9a3788dae883b63efa000752baac
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 07:42:43 +0200

    Frontend: Assigning properties and constructor of LabeledProgressBar

--- flutter_app/lib/widgets/labeled.progress.bar.dart
+++ flutter_app/lib/widgets/labeled.progress.bar.dart
@@ -1,9 +1,18 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class LabeledProgressBar extends StatelessWidget {
+  final String label;
+  final double progress;
+  final Color progress_color;
+
+  LabeledProgressBar({
+    required this.label,
+    required this.progress,
+    required this.progress_color,
+  });
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column();
   }
 }
