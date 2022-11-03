bb3592dfd64973fbdde8a23e08255d94d3074390
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 08:35:32 +0200

    Frontend: Assigning properties and constructor of CostumedButton

--- flutter_app/lib/widgets/costumed.button.dart
+++ flutter_app/lib/widgets/costumed.button.dart
@@ -1,7 +1,22 @@
+import 'package:flutter/animation.dart';
 import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
 class CostumedButton extends StatelessWidget {
+  final double height;
+  final double width;
+  final double raduis;
+  final Color background;
+  final String text;
+
+  CostumedButton({
+    required this.height,
+    required this.width,
+    required this.raduis,
+    required this.background,
+    required this.text,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container();
