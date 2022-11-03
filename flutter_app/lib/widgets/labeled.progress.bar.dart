4665ae50b4faf1bd70b411814801891552089148
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 07:43:16 +0200

    Frontend: Designing LabeledProgressBar elements

--- flutter_app/lib/widgets/labeled.progress.bar.dart
+++ flutter_app/lib/widgets/labeled.progress.bar.dart
@@ -13,6 +13,36 @@ class LabeledProgressBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      children: [
+        Text(
+          label,
+          style: Theme.of(context).textTheme.titleMedium,
+        ),
+        Stack(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 15),
+              height: 40,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: Theme.of(context).primaryColorLight,
+                borderRadius: BorderRadius.circular(15),
+              ),
+            ),
+            FractionallySizedBox(
+              widthFactor: progress,
+              child: Container(
+                margin: EdgeInsets.symmetric(vertical: 15),
+                height: 40,
+                decoration: BoxDecoration(
+                    color: progress_color,
+                    borderRadius: BorderRadius.circular(10)),
+              ),
+            )
+          ],
+        )
+      ],
+    );
   }
 }
