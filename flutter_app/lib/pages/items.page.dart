b10c1378fa41e649637d842a725671e68dd222f7
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 21:58:11 +0200

    Frontend: Creating items.page.dart

---
+++ flutter_app/lib/pages/items.page.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ItemsPage extends StatefulWidget {
+  @override
+  State<ItemsPage> createState() => _ItemsPageState();
+}
+
+class _ItemsPageState extends State<ItemsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
