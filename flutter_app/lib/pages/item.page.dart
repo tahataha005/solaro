9c7409c4940f1003c304de64b8693fea75c53cb0
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 16:18:08 +0200

    Frontend: Adding bottom section to item page

--- flutter_app/lib/pages/item.page.dart
+++ flutter_app/lib/pages/item.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/costumed.button.dart';
 
 class ItemPage extends StatefulWidget {
   const ItemPage({super.key});
@@ -8,60 +9,66 @@ class ItemPage extends StatefulWidget {
 }
 
 class _ItemPageState extends State<ItemPage> {
+  bool status = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20),
-        width: double.infinity,
-        child: Column(
-          children: [
-            SizedBox(
-              height: MediaQuery.of(context).padding.top,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            padding: EdgeInsets.all(20),
+            width: double.infinity,
+            child: Column(
               children: [
-                IconButton(
-                  onPressed: () {},
-                  icon: Icon(Icons.arrow_back),
+                SizedBox(
+                  height: MediaQuery.of(context).padding.top,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.arrow_back),
+                    ),
+                  ],
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Container(
+                  width: double.infinity,
+                  height: MediaQuery.of(context).size.width - 20,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+                    color: Colors.white,
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey,
+                        offset: Offset(0, 3), //(x,y)
+                        blurRadius: 5,
+                      ),
+                    ],
+                  ),
+                  child: Stack(
+                    fit: StackFit.expand,
+                    children: [
+                      ClipRRect(
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(30),
+                        ),
+                        child: Image.asset(
+                          "assets/images/Item 1.png",
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ],
             ),
-            SizedBox(
-              height: 50,
-            ),
-            Container(
-              width: double.infinity,
-              height: MediaQuery.of(context).size.width - 20,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                color: Colors.white,
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.grey,
-                    offset: Offset(0, 3), //(x,y)
-                    blurRadius: 5,
-                  ),
-                ],
-              ),
-              child: Stack(
-                fit: StackFit.expand,
-                children: [
-                  ClipRRect(
-                    borderRadius: BorderRadius.all(
-                      Radius.circular(30),
-                    ),
-                    child: Image.asset(
-                      "assets/images/Item 1.png",
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ],
-              ),
-            )
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
