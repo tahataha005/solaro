6be45c9dfb6cf01fc8fdc9d5e6c94cbfa8f9a433
tahataha005 <taha.f.taha0@gmail.com>
2022-11-03 07:25:12 +0200

    Frontend: Assigning properties and constructor of ItemCard

--- flutter_app/lib/widgets/item.card.dart
+++ flutter_app/lib/widgets/item.card.dart
@@ -1,7 +1,12 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class ItemCard extends StatelessWidget {
+  final String item_name;
+
+  ItemCard({
+    required this.item_name,
+  });
+
   @override
   Widget build(BuildContext context) {
     return Container();
