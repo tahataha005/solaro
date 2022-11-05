5ed09ffa1e5c04fd3a51fbfd88cba33d6bb53a2e
tahataha005 <taha.f.taha0@gmail.com>
2022-11-05 07:46:48 +0200

    Frontend: Adding a multiprovider at the top level of main.dart

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -4,6 +4,9 @@ import 'package:flutter_app/pages/details.page.dart';
 import 'package:flutter_app/pages/item.page.dart';
 import 'package:flutter_app/pages/items.page.dart';
 import 'package:flutter_app/pages/main.page.dart';
+import 'package:flutter_app/models/item.model.dart';
+import 'package:flutter_app/providers/items.provider.dart';
+import 'package:provider/provider.dart';
 
 import './tools/color.constructor.dart';
 import './pages/register/register.page.dart';
@@ -15,59 +18,66 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(
-        primarySwatch: createMaterialColor(Color.fromRGBO(0, 114, 187, 1)),
-        accentColor: Color.fromRGBO(255, 186, 73, 1),
-        primaryColorLight: Color.fromRGBO(233, 235, 248, 1),
-        fontFamily: "Inter",
-        textTheme: ThemeData.light().textTheme.copyWith(
-              labelMedium: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 20,
-                color: Colors.white,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: Items(),
+        ),
+      ],
+      child: MaterialApp(
+        theme: ThemeData(
+          primarySwatch: createMaterialColor(Color.fromRGBO(0, 114, 187, 1)),
+          accentColor: Color.fromRGBO(255, 186, 73, 1),
+          primaryColorLight: Color.fromRGBO(233, 235, 248, 1),
+          fontFamily: "Inter",
+          textTheme: ThemeData.light().textTheme.copyWith(
+                labelMedium: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+                labelLarge: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                titleLarge: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 32,
+                  fontWeight: FontWeight.bold,
+                ),
+                titleMedium: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+                bodySmall: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 16,
+                ),
+                bodyMedium: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 20,
+                ),
+                bodyLarge: const TextStyle(
+                  fontFamily: "Inter",
+                  fontSize: 24,
+                ),
               ),
-              labelLarge: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.white,
-              ),
-              titleLarge: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-              ),
-              titleMedium: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-              bodySmall: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 16,
-              ),
-              bodyMedium: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 20,
-              ),
-              bodyLarge: const TextStyle(
-                fontFamily: "Inter",
-                fontSize: 24,
-              ),
-            ),
+        ),
+        home: RegisterPage(),
+        routes: {
+          "/first": (context) => RegisterPage(),
+          "/landing": (context) => LandingPage(),
+          "/notifications": (context) => NotificationsPage(),
+          "/main": (context) => MainPage(),
+          "/details": (context) => DetailsPage(),
+          "/item": (context) => ItemPage(),
+          "/items": (context) => ItemsPage(),
+          "/create": (context) => CreateItemPage()
+        },
       ),
-      home: RegisterPage(),
-      routes: {
-        "/first": (context) => RegisterPage(),
-        "/landing": (context) => LandingPage(),
-        "/notifications": (context) => NotificationsPage(),
-        "/main": (context) => MainPage(),
-        "/details": (context) => DetailsPage(),
-        "/item": (context) => ItemPage(),
-        "/items": (context) => ItemsPage(),
-        "/create": (context) => CreateItemPage()
-      },
     );
   }
 }
