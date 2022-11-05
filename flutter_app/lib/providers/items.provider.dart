3a0f2928bf083947894159129326a87c0b5665b9
tahataha005 <taha.f.taha0@gmail.com>
2022-11-05 07:48:26 +0200

    Frontend: Creating a temporary list of items in items provider

--- flutter_app/lib/providers/items.provider.dart
+++ flutter_app/lib/providers/items.provider.dart
@@ -1,4 +1,37 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/models/item.model.dart';
 
-class Items extends ChangeNotifier {}
+class Items extends ChangeNotifier {
+  List<Item> _items = [
+    Item(
+      item_name: "Refrigerator",
+      ideal_consumption: 3,
+      live_consumption: 2.8,
+      status: true,
+    ),
+    Item(
+      item_name: "Lights",
+      ideal_consumption: 1.5,
+      live_consumption: 1.4,
+      status: true,
+    ),
+    Item(
+      item_name: "Dryer",
+      ideal_consumption: 100,
+      live_consumption: 101,
+      status: true,
+    ),
+    Item(
+      item_name: "TV",
+      ideal_consumption: 4.5,
+      live_consumption: 0,
+      status: false,
+    ),
+    Item(
+      item_name: "Heater",
+      ideal_consumption: 2,
+      live_consumption: 0,
+      status: false,
+    ),
+  ];
+}
