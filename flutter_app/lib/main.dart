2c36d8d30e20f9d90e609bf0964996f02ef69fa4
tahataha005 <taha.f.taha0@gmail.com>
2022-11-06 22:31:39 +0200

    Backend: Adding user provider

--- flutter_app/lib/main.dart
+++ flutter_app/lib/main.dart
@@ -67,11 +67,6 @@ class MyApp extends StatelessWidget {
                   fontFamily: "Inter",
                   fontSize: 24,
                 ),
-                displayMedium: const TextStyle(
-                  color: Color.fromARGB(255, 183, 28, 28),
-                  fontFamily: "Inter",
-                  fontSize: 16,
-                ),
               ),
         ),
         home: RegisterPage(),
