ce4b716be3d2bc38a014824078ed89bc6310b8cf
tahataha005 <taha.f.taha0@gmail.com>
2022-11-06 21:45:44 +0200

    Backend: Defining properties and and constructor of User provider

--- flutter_app/lib/providers/user.provider.dart
+++ flutter_app/lib/providers/user.provider.dart
@@ -1,3 +1,11 @@
 import 'package:flutter/cupertino.dart';
 
-class User extends ChangeNotifier {}
+class User extends ChangeNotifier {
+  String userId;
+  String token;
+
+  User({
+    required this.userId,
+    required this.token,
+  });
+}
