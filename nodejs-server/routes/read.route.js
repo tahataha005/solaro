fc5c14d607a6e6bf8088cdc5d56bcc015a9c4636
tahataha005 <taha.f.taha0@gmail.com>
2022-11-07 20:55:52 +0200

    Backend: Assigning route for getUser function in read.routes.js

--- nodejs-server/routes/read.route.js
+++ nodejs-server/routes/read.route.js
@@ -4,11 +4,14 @@ const router = Router();
 
 const {
     getSolarStats,
+    getUser,
     // getAllItems,
     // getItem,
 } = require("../controllers/read.controller.js");
 
 router.get("/system/:user_id/:system_id", getSolarStats);
+router.get("/:user_id", getUser);
+
 // router.get("/item/:item_id", getItem);
 // router.get("/items/:user_id/:system_name", getAllItems);
 
