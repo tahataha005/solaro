d4f4bd58541bd4f0fb36c4c12a39f0e55c0750fe
tahataha005 <taha.f.taha0@gmail.com>
2022-11-07 11:42:49 +0200

    Frontend: Defining systems properties and constructor

--- flutter_app/lib/providers/systems.provider.dart
+++ flutter_app/lib/providers/systems.provider.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
-class Systems extends ChangeNotifier {}
+class Systems extends ChangeNotifier {
+  List systems = [];
+
+  Systems({
+    required this.systems,
+  });
+}
