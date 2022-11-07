1c9635c558980c03863d66b3301ac379cfa74e1e
tahataha005 <taha.f.taha0@gmail.com>
2022-11-07 11:31:06 +0200

    Frontend: Adding main elements of modal sheet

--- flutter_app/lib/widgets/modal.sheet.dart
+++ flutter_app/lib/widgets/modal.sheet.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/widgets/costumed.button.dart';
 
 class ModalSheet extends StatefulWidget {
   @override
@@ -8,6 +9,35 @@ class ModalSheet extends StatefulWidget {
 class _ModalSheetState extends State<ModalSheet> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      width: double.infinity,
+      height: MediaQuery.of(context).size.width,
+      padding: EdgeInsets.symmetric(horizontal: 50),
+      color: Colors.white,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Text(
+            "Add Solar System",
+          ),
+          Container(
+            child: Form(
+              child: Column(
+                children: [
+                  TextFormField(),
+                  TextFormField(),
+                ],
+              ),
+            ),
+          ),
+          TextButton(
+            child: Text(
+              "ADD SYSTEM",
+            ),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
   }
 }
