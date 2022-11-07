53bcd10e637b56bfef2dab590913d339ab556bd8
tahataha005 <taha.f.taha0@gmail.com>
2022-11-07 10:57:05 +0200

    Frontend: Creating modal.sheet.dart in widgets

---
+++ flutter_app/lib/widgets/modal.sheet.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ModalSheet extends StatefulWidget {
+  @override
+  State<ModalSheet> createState() => _ModalSheetState();
+}
+
+class _ModalSheetState extends State<ModalSheet> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
