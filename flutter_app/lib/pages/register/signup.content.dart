23a306150b7d71702ef4dc8743f20d121c2241b6
tahataha005 <taha.f.taha0@gmail.com>
2022-11-07 07:19:11 +0200

    Backend: Defining and assigning text controllers and adding next text action to text fields in signup content

--- flutter_app/lib/pages/register/signup.content.dart
+++ flutter_app/lib/pages/register/signup.content.dart
@@ -9,6 +9,9 @@ class SignUp extends StatefulWidget {
 enum UserType { controller, viewer }
 
 class _SignUpState extends State<SignUp> {
+  final _email = TextEditingController();
+  final _password = TextEditingController();
+  final _confirm = TextEditingController();
   UserType? _userType = UserType.viewer;
 
   @override
@@ -25,6 +28,8 @@ class _SignUpState extends State<SignUp> {
                   mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
                     TextFormField(
+                      controller: _email,
+                      textInputAction: TextInputAction.next,
                       decoration: InputDecoration(
                         label: Text(
                           "Email",
@@ -33,6 +38,8 @@ class _SignUpState extends State<SignUp> {
                       ),
                     ),
                     TextFormField(
+                      controller: _password,
+                      textInputAction: TextInputAction.next,
                       decoration: InputDecoration(
                         label: Text(
                           "Password",
@@ -41,6 +48,8 @@ class _SignUpState extends State<SignUp> {
                       ),
                     ),
                     TextFormField(
+                      controller: _confirm,
+                      textInputAction: TextInputAction.next,
                       decoration: InputDecoration(
                         label: Text(
                           "Confirm Password",
