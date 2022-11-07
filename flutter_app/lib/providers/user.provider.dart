c197c7efff664ba9a1b8690d56285b2504b28b54
tahataha005 <taha.f.taha0@gmail.com>
2022-11-07 22:39:55 +0200

    Backend: Defining properties and a getter for user provider

--- flutter_app/lib/providers/user.provider.dart
+++ flutter_app/lib/providers/user.provider.dart
@@ -1,3 +1,9 @@
 import 'package:flutter/cupertino.dart';
 
-class User extends ChangeNotifier {}
+class User extends ChangeNotifier {
+  Map user = {};
+
+  Map get _user {
+    return {...user};
+  }
+}
