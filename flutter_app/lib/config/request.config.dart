36c43f96a7cc3b70d2dcdca97c76a4a7ced46368
tahataha005 <taha.f.taha0@gmail.com>
2022-11-08 22:31:23 +0200

    Frontend: Assigning listen to false when getting token in request configuration

--- flutter_app/lib/config/request.config.dart
+++ flutter_app/lib/config/request.config.dart
@@ -2,11 +2,11 @@ import 'package:flutter_app/providers/auth.provider.dart';
 import 'package:provider/provider.dart';
 
 class RequestConfig {
-  static String url = "172.20.10.2:8000";
+  static String url = "192.168.1.12:8000";
   Map<String, String> headers = {"Content-Type": "application/json"};
 
   Map<String, String> getHeaders(context) {
-    final token = Provider.of<Auth>(context).getToken;
+    final token = Provider.of<Auth>(context, listen: false).getToken;
 
     if (token != null) {
       return {
