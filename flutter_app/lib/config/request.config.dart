7f8d0c30958eb0e05a5892be8b308b5706f21d6b
tahataha005 <taha.f.taha0@gmail.com>
2022-11-08 15:12:40 +0200

    Frontend: Creating headers property and getter

--- flutter_app/lib/config/request.config.dart
+++ flutter_app/lib/config/request.config.dart
@@ -1,3 +1,11 @@
+import 'package:flutter_app/providers/auth.provider.dart';
+import 'package:provider/provider.dart';
+
 class RequestConfig {
   static String url = "172.20.10.2:8000";
+
+  Map<String, String> getHeaders(context) {
+    final token = Provider.of<Auth>(context).getToken;
+    final Map<String, String> headers;
+  }
 }
