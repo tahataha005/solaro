81a8715e89f7cc2f966ea6cc4e75dd3406239603
tahataha005 <taha.f.taha0@gmail.com>
2022-11-08 21:15:09 +0200

    Frontend: Returning token in headers if present in request configuration

--- flutter_app/lib/config/request.config.dart
+++ flutter_app/lib/config/request.config.dart
@@ -3,9 +3,18 @@ import 'package:provider/provider.dart';
 
 class RequestConfig {
   static String url = "172.20.10.2:8000";
+  Map<String, String> headers = {"Content-Type": "application/json"};
 
   Map<String, String> getHeaders(context) {
     final token = Provider.of<Auth>(context).getToken;
-    final Map<String, String> headers;
+
+    if (token != null) {
+      return {
+        ...headers,
+        "Authorization": "Bearer $token",
+      };
+    }
+
+    return headers;
   }
 }
