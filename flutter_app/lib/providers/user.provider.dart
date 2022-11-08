02184aaacc0e4ea7d1f813d517c895d304d3e10f
tahataha005 <taha.f.taha0@gmail.com>
2022-11-08 05:09:43 +0200

    Frontend: Handling errors in setSystems in user provider

--- flutter_app/lib/providers/user.provider.dart
+++ flutter_app/lib/providers/user.provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/models/exception.model.dart';
 import 'package:flutter_app/providers/systems.provider.dart';
 import 'package:flutter_app/tools/request.dart';
 
@@ -12,10 +13,17 @@ class User extends ChangeNotifier {
     try {
       final response =
           await sendRequest(route: "/read/63669e05e464e3fb910146c8");
+
+      if (response["message"] != null) {
+        throw HttpException(response["message"]);
+      }
+
       id = response["user_id"];
       email = response["email"];
       userType = response["userType"];
       systems = response["system"];
-    } catch (e) {}
+    } catch (e) {
+      rethrow;
+    }
   }
 }
