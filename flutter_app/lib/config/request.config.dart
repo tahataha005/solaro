1f229db35f59403764d7a33915d7e207d50351d7
tahataha005 <taha.f.taha0@gmail.com>
2022-11-10 21:04:50 +0200

    Frontend: Adding created item in loadItems to list of items in item provider

--- flutter_app/lib/config/request.config.dart
+++ flutter_app/lib/config/request.config.dart
@@ -2,7 +2,7 @@ import 'package:flutter_app/providers/auth.provider.dart';
 import 'package:provider/provider.dart';
 
 class RequestConfig {
-  static String url = "192.168.1.12:8000";
+  static String url = "192.168.44.106:8000";
   Map<String, String> headers = {"Content-Type": "application/json"};
 
   Map<String, String> getHeaders(context) {
