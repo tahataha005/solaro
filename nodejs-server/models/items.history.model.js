0e3bad2d989b96a35ab5cb2b3baabc9b63784391
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 09:44:25 +0200

    Backend: Creating itemshistory schema in item.history.model.js

--- nodejs-server/models/items.history.model.js
+++ nodejs-server/models/items.history.model.js
@@ -1,1 +1,17 @@
 const mongoose = require("mongoose");
+
+const itemsHistorySchema = mongoose.Schema({
+    item_id: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Item",
+    },
+    consumption: {
+        type: Number,
+        required: "peak is required",
+    },
+    timestamp: {
+        type: Date,
+        required: "date is required",
+        default: Date.now,
+    },
+});
