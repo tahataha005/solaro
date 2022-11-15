4d7552f8ec09d2e18b183e134e1c458b3c481b1c
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 07:34:10 +0200

    Backend: Emmiting connection message on connection

--- nodejs-server/config/socket.config.js
+++ nodejs-server/config/socket.config.js
@@ -2,6 +2,11 @@ const socketInit = (app, address) => {
     const http = require("http").Server(app);
     const io = require("socket.io")(http);
 
+    io.on("connection", socket => {
+        console.log("user server started");
+        socket.emit("message", "connected to server");
+    });
+
     //Listening on port and address
     http.listen(process.env.SOCKET_PORT, address, () => {
         console.log(
