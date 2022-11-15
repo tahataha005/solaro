611c6fd8ad9f20d5a2707207a3fceb1dddf60a5c
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 10:45:26 +0200

    Backend: Creating insertSystemData in data.controller.js

--- nodejs-server/controllers/data.controller.js
+++ nodejs-server/controllers/data.controller.js
@@ -1,7 +1,7 @@
 const ItemHistory = require("../models/items.history.model.js");
 const SolarHistory = require("../models/solar.history.model.js");
 
-//Insert data
+//Insert item history data
 const insertItemData = async (req, res) => {
     try {
         //Destructuring req data
@@ -25,7 +25,7 @@ const insertItemData = async (req, res) => {
     }
 };
 
-//Get history according to data
+//Get item history according to id
 const getItemData = async (req, res) => {
     try {
         //Destructuring req data
@@ -41,6 +41,7 @@ const getItemData = async (req, res) => {
     }
 };
 
+//Insert system history data
 const insertSystemData = async (req, res) => {
     //Destructuring req data
     const { system_id, charging, consumption, time } = req.body;
@@ -65,4 +66,25 @@ const insertSystemData = async (req, res) => {
     }
 };
 
-module.exports = { insertItemData, getItemData };
+//Get system history according to id
+const getSystemData = async (req, res) => {
+    try {
+        //Destructuring req data
+        const { item_id } = req.params;
+
+        //Getting documents accoring to item id
+        const retrieved = await ItemHistory.find({ item_id });
+
+        //Returning retrieved data
+        res.status(200).json(retrieved);
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+};
+
+module.exports = {
+    insertItemData,
+    getItemData,
+    insertSystemData,
+    getSystemData,
+};
