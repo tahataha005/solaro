8fd53057c55020a1c56c22e3061d000c65b2528c
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 23:46:36 +0200

    Frontend: Creating ColumnChart in widgets

---
+++ flutter_app/lib/widgets/column.chart.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class ColumnChart extends StatelessWidget {
+  final List data;
+
+  ColumnChart({required this.data});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
