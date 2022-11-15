f70f5fea505dde9729c5a9c50bb42cc09cc5f2dd
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 07:39:15 +0200

    Backend: Informing socket disconnection

--- nodejs-server/config/socket.config.js
+++ nodejs-server/config/socket.config.js
@@ -2,9 +2,15 @@ const socketInit = (app, address) => {
     const http = require("http").Server(app);
     const io = require("socket.io")(http);
 
+    //On Socket connection
     io.on("connection", socket => {
         console.log("user server started");
         socket.emit("message", "connected to server");
+
+        //On socket disconnect
+        socket.on("disconnect", function () {
+            console.log("user disconnected");
+        });
     });
 
     //Listening on port and address
