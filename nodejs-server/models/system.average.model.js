015fb46e462bfbd059f77347fe4eff6d6fff6caa
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 22:53:12 +0200

    Backend: Creating system average schema in system.average.model.js

--- nodejs-server/models/system.average.model.js
+++ nodejs-server/models/system.average.model.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+
+const systemAveragesSchema = mongoose.Schema({
+    system_id: {
+        type: String,
+        required: "system id required",
+    },
+    day: {
+        type: Object,
+        required: "date required",
+    },
+    avg_consumption: {
+        type: Number,
+        required: "average consumption required",
+    },
+});
