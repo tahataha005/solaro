03ace853138ab81c0b548926b7a555a36cef2548
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 07:30:37 +0200

    Backend: Listening scket on port and address

--- nodejs-server/config/socket.config.js
+++ nodejs-server/config/socket.config.js
@@ -1,6 +1,13 @@
 const socketInit = (app, address) => {
     const http = require("http").Server(app);
     const io = require("socket.io")(http);
+
+    //Listening on port and address
+    http.listen(process.env.SOCKET_PORT, address, () => {
+        console.log(
+            `Socket is running on PORT ${process.env.SOCKET_PORT} / IP Address ${address}`
+        );
+    });
 };
 
 module.exports = socketInit;
