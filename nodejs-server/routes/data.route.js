4138c2732ce21c1c11f40677e020f575dc027bce
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 10:03:22 +0200

    Backend: Assigning root to insertData in data.route.js

--- nodejs-server/routes/data.route.js
+++ nodejs-server/routes/data.route.js
@@ -3,4 +3,10 @@ const Router = require("express");
 //Creating a router
 const router = Router();
 
+//Importing functions from controllers
+const { insertData } = require("../controllers/data.controller");
+
+//Creating routes
+router.post("/", insertData);
+
 module.exports = router;
