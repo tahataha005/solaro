509fd0a25e284b657c4ab514b0a5d456095021b6
tahataha005 <taha.f.taha0@gmail.com>
2022-11-15 22:55:08 +0200

    Backend: Creating item average schema in item.average.model.js

--- nodejs-server/models/item.average.model.js
+++ nodejs-server/models/item.average.model.js
@@ -1,1 +1,16 @@
 const mongoose = require("mongoose");
+
+const itemAveragesSchema = mongoose.Schema({
+    item_id: {
+        type: String,
+        required: "item id required",
+    },
+    day: {
+        type: Object,
+        required: "date required",
+    },
+    avg_consumption: {
+        type: Number,
+        required: "average consumption required",
+    },
+});
