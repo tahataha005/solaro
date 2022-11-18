83022f3edde76ffaeb2fe2eb9320e4601ce67465
tahataha005 <taha.f.taha0@gmail.com>
2022-11-18 15:40:29 +0200

    Backend: Creating last week's date in cleaner.scheduller.js

--- nodejs-server/schedullers/cleaner.scheduller.js
+++ nodejs-server/schedullers/cleaner.scheduller.js
@@ -2,4 +2,7 @@ const ItemHistory = require("../models/items.history.model.js");
 const SolarHistory = require("../models/solar.history.model");
 const cron = require("node-cron");
 
+const start = new Date(new Date().getTime());
+start.setDate(start.getDate() - 7);
+
 cron.schedule("0 0 0 * * *", () => {});
