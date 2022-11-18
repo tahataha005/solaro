513a7434d3bedb835312c8dba8bd13fab599b85f
tahataha005 <taha.f.taha0@gmail.com>
2022-11-18 16:05:43 +0200

    Backend: Saving new record in saveData in save.data.scheduller.js

--- nodejs-server/schedullers/save.data.scheduller.js
+++ nodejs-server/schedullers/save.data.scheduller.js
@@ -5,6 +5,17 @@ const cron = require("node-cron");
 const saveDataScheduller = reading => {
     const saveData = async reading => {
         try {
+            //Creating new item history record
+            const record = new ItemHistory();
+
+            //Assigning properties of new record
+            const date = new Date();
+            record.item_id = reading.item_id;
+            record.consumption = reading.consumption;
+            record.timestamp = date;
+
+            //Saving record
+            await record.save();
         } catch (err) {
             console.log(err);
         }
