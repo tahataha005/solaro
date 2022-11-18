62d358eeeb79b425b8795ea319dc8cd767eddec9
tahataha005 <taha.f.taha0@gmail.com>
2022-11-18 16:02:39 +0200

    Backend: Assigning node scheduller in saveDataScheduller function in save.data.scheduller.js

--- nodejs-server/schedullers/save.data.scheduller.js
+++ nodejs-server/schedullers/save.data.scheduller.js
@@ -2,6 +2,8 @@ const ItemHistory = require("../models/items.history.model.js");
 const SolarHistory = require("../models/solar.history.model");
 const cron = require("node-cron");
 
-const saveDataScheduller = reading => {};
+const saveDataScheduller = reading => {
+    cron.schedule("0 0 * * * *", () => {});
+};
 
 module.exports = { saveDataScheduller };
