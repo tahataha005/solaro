2dd711c4cae96b0fd54ee2a9d4929c45664db0f7
tahataha005 <taha.f.taha0@gmail.com>
2022-11-18 15:38:55 +0200

    Backend: Creating a node schedule at daily middlenight in cleaner.scheduller.js

--- nodejs-server/schedullers/cleaner.scheduller.js
+++ nodejs-server/schedullers/cleaner.scheduller.js
@@ -1,3 +1,5 @@
 const ItemHistory = require("../models/items.history.model.js");
 const SolarHistory = require("../models/solar.history.model");
 const cron = require("node-cron");
+
+cron.schedule("0 0 0 * * *", () => {});
