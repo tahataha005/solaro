b6f49a6077ac60ce6d023e0db0c4a5e6f9d8067a
tahataha005 <taha.f.taha0@gmail.com>
2022-11-18 14:10:34 +0200

    Backend: Creating average.scheduller.js in schedullers

---
+++ nodejs-server/schedullers/average.scheduller.js
@@ -0,0 +1,7 @@
+const ItemHistory = require("../models/items.history.model.js");
+const SolarAverage = require("../models/system.average.model");
+const ItemAverage = require("../models/item.average.model");
+const cron = require("node-cron");
+const SolarHistory = require("../models/solar.history.model");
+
+const weekday = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
