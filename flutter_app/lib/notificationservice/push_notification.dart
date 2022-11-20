655d0e75916059ab6ac2bc6dc7ba0de7490ec8a7
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 15:01:38 +0200

    Frontend: Creating push_notification.dart in notificationservices

---
+++ flutter_app/lib/notificationservice/push_notification.dart
@@ -0,0 +1,5 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_app/notificationservice/local_notification_service.dart';
+
+class PushNotification {}
