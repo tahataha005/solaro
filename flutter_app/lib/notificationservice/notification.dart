aff76013134e241a03daff5af7104128ee01a946
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 15:00:26 +0200

    Frontend: Creating getToken in notification.dart

--- flutter_app/lib/notificationservice/notification.dart
+++ flutter_app/lib/notificationservice/notification.dart
@@ -1,1 +1,9 @@
-class MyNotification {}
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class MyNotification {
+  static Future getToken() async {
+    await FirebaseMessaging.instance.getToken().then((token) {
+      print("token: $token");
+    });
+  }
+}
