0190f460604074a66829f07a997be1cfaa804f48
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 14:50:30 +0200

    Frontend: Creating initialize setting in local notification service

--- flutter_app/lib/notificationservice/local_notification_service.dart
+++ flutter_app/lib/notificationservice/local_notification_service.dart
@@ -1,6 +1,15 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class LocalNotificationService {
   static final FlutterLocalNotificationsPlugin _notificationsPlugin =
       FlutterLocalNotificationsPlugin();
+
+  static void initialize() {
+    //InitializationSettings for Android
+    const InitializationSettings initializationSettings =
+        InitializationSettings(
+      android: AndroidInitializationSettings("@mipmap/ic_launcher"),
+    );
+  }
 }
