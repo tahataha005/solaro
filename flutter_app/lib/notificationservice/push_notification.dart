68e72438c74efc414ef4751d4ec26a24e6b6b65b
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 15:03:15 +0200

    Frontend: Creating get initial message in push notification

--- flutter_app/lib/notificationservice/push_notification.dart
+++ flutter_app/lib/notificationservice/push_notification.dart
@@ -2,4 +2,17 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_app/notificationservice/local_notification_service.dart';
 
-class PushNotification {}
+class PushNotification {
+  static void pushNotification(context) {
+    void getInitialMessage() {
+      FirebaseMessaging.instance.getInitialMessage().then(
+        (message) {
+          print("FirebaseMessaging.instance.getInitialMessage");
+          if (message != null) {
+            print("New Notification");
+          }
+        },
+      );
+    }
+  }
+}
