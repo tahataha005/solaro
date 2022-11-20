a8a2410c4963f9b9ead0c40b26bc36565de20876
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 14:48:47 +0200

    Frontend: Creating LocalNotificationsPlugin in local notifiaction services

--- flutter_app/lib/notificationservice/local_notification_service.dart
+++ flutter_app/lib/notificationservice/local_notification_service.dart
@@ -1,1 +1,6 @@
-class LocalNotificationService {}
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+
+class LocalNotificationService {
+  static final FlutterLocalNotificationsPlugin _notificationsPlugin =
+      FlutterLocalNotificationsPlugin();
+}
