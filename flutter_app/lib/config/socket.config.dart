8da1c1a01dfb42dcd4ccb3c7f18b5a4e3379c275
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 13:15:44 +0200

    Frontend: Initializing socket in socket configuration

--- flutter_app/lib/config/socket.config.dart
+++ flutter_app/lib/config/socket.config.dart
@@ -1,1 +1,8 @@
-class Socket {}
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class Socket {
+  static IO.Socket socket = IO.io("http://192.168.44.106:3000", {
+    "transports": ["websocket"],
+    "autoConnect": true,
+  });
+}
