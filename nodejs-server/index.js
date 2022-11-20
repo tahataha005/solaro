0fa73d3e8e4db60394237d396705d33a0841f803
tahataha005 <taha.f.taha0@gmail.com>
2022-11-20 15:14:18 +0200

    Backend: Assigning rouet for public folder in index.js

--- nodejs-server/index.js
+++ nodejs-server/index.js
@@ -1,18 +1,24 @@
 const express = require("express");
+const path = require("path");
+const ip = require("ip");
 require("dotenv").config();
 require("./config/db.config.js");
 require("./schedullers/average.scheduller");
 require("./schedullers/cleaner.scheduller");
 require("./schedullers/save.data.scheduller");
 
-const ip = require("ip");
+//Getting current network ip address
 const address = ip.address();
 
 //Initilizing app as express method
 const app = express();
 app.use(express.json());
 
+//Initializing socket
 require("./config/socket.config")(app, address);
+
+//Assigning rouet for public folder
+app.use("/static", express.static(path.join(__dirname, "./public")));
 
 //Listening on server port and logging status
 app.listen(process.env.SERVER_PORT, address, err => {
