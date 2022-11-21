930af857bd8846d846cf406a214c415b9bceaefe
tahataha005 <taha.f.taha0@gmail.com>
2022-11-21 12:59:39 +0200

    Frontend: Creating addNotification method in notifications provider

--- flutter_app/lib/providers/notifications.provider.dart
+++ flutter_app/lib/providers/notifications.provider.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/foundation.dart';
+import 'package:flutter_app/models/notifications.model.dart';
 
 class Notifications with ChangeNotifier {
   List notifications = [];
 
   List get getNotifications {
     return notifications;
+  }
+
+  void addNotification(title, body, data, time) {
+    final newNotification = Notification(
+      system_name: title.split(":")[0],
+      title: title.split(":")[1],
+      content: body,
+      time: time,
+    );
+    notifications.add(newNotification);
+
+    notifyListeners();
   }
 }
