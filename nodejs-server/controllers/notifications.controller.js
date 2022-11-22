d9cbf2b2c934da3676d0603c908be9450f528109
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 18:03:30 +0200

    Backend: Creating sendNotification function in notifications.controller.js

--- nodejs-server/controllers/notifications.controller.js
+++ nodejs-server/controllers/notifications.controller.js
@@ -1,1 +1,14 @@
 const { admin } = require("../config/notifications/notification.config.js");
+
+const sendNotification = async (req, res) => {
+    try {
+        //Destructuring req data
+        const { registrationToken, title, body } = req.body;
+    } catch (error) {
+        res.status(400).json(error.message);
+    }
+};
+
+module.exports = {
+    sendNotification,
+};
