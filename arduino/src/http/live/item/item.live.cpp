516e19ed4b54fd843eae9f1a3f8db03058cc025e
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 20:01:41 +0200

    Arduino: Creating itemLive function in item.live.cpp

--- arduino/src/http/live/item/item.live.cpp
+++ arduino/src/http/live/item/item.live.cpp
@@ -1,1 +1,13 @@
 #include "item.live.h"
+
+//Setup live item data reading
+void liveItem(u_int8_t pin, const char* id,double idealConsumption, bool save) {
+
+    //Initialize variables
+    WiFiClient wifiClient;
+    HTTPClient http;
+    StaticJsonDocument<256> doc;
+    const char* data = "{}";
+    String result;
+
+}
