7d569c8d49424bf62e1e357bd8b62a2dce86f2c4
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 20:17:36 +0200

    Arduino: Creating liveSystem in system.live.cpp

--- arduino/src/http/live/solarsystem/system.live.cpp
+++ arduino/src/http/live/solarsystem/system.live.cpp
@@ -1,2 +1,12 @@
 #include "system.live.h"
 
+//Setup live system data reading
+void liveSystem(uint8_t consumptionPin, uint8_t chargingPin, const char* id,bool save){
+
+    //Initialize variables
+    WiFiClient wifiClient;
+    HTTPClient http;
+    StaticJsonDocument<256> doc;
+    const char* data = "{}";
+    String result;
+}
