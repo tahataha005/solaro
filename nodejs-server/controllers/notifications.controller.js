d46e8aaf6fc6b09f26dbf743874f8e90ab647923
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 18:06:04 +0200

    Backend: Structuring a new message in sendNotification in notifications.controller.js

--- nodejs-server/controllers/notifications.controller.js
+++ nodejs-server/controllers/notifications.controller.js
@@ -4,6 +4,18 @@ const sendNotification = async (req, res) => {
     try {
         //Destructuring req data
         const { registrationToken, title, body } = req.body;
+
+        //Creating a message
+        const message = {
+            notification: {
+                title: title,
+                body: body,
+            },
+            token: registrationToken,
+        };
+
+        //Returning success message
+        res.status(200).json("Notification sent");
     } catch (error) {
         res.status(400).json(error.message);
     }
