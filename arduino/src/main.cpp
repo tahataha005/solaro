e5e6387f768f1bc0f6e00c981539f235fabb106e
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 19:57:14 +0200

    Arduino: Checking wifi connection status in main.cpp

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -10,6 +10,13 @@ void setup() {
   //Connect to wifi
   setupWifi();
 
+  //Check if wifi is connected
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Error in WiFi connection");
+  } else {
+    Serial.println("Error in WiFi connection");
+  }
+
 }
 
 //Main Loop function (Executed in a loop)
