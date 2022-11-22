e7bd2d490a9087c6c686fd739751139f12974f7f
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 19:51:48 +0200

    Arduino: Connecting to wifi un setupWifi in wifi.cpp

--- arduino/src/wifi/wifi.cpp
+++ arduino/src/wifi/wifi.cpp
@@ -6,5 +6,19 @@ const char* password = "kaykaykay1";
 
 //Setup wifi
 void setupWifi(){
+  //Connect to wifi
+  WiFi.begin(ssid,password);
 
+  //Wait for connection
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    //Wait 1 second
+    delay(1000);
+    Serial.println("*");
+  }
+
+  //Display IP address
+  if (WiFi.status() == WL_CONNECTED) {
+    Serial.println("Connected to "+(WiFi.localIP()).toString());
+  }
 };
