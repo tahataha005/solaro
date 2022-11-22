87b916cd73ec16bbf96c88fccda233b3f8f88641
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 17:59:13 +0200

    Backend: Importing function from data.controller.js

--- nodejs-server/routes/arduino.route.js
+++ nodejs-server/routes/arduino.route.js
@@ -3,4 +3,12 @@ const Router = require("express");
 //Creating a router
 const router = Router();
 
+//Importing functions from controllers
+const {
+    liveItem,
+    liveSystem,
+    insertItemData,
+    insertSystemData,
+} = require("../controllers/data.controller.js");
+
 module.exports = router;
