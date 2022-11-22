77e521d338a66a08500a466e85920d59fc12d8da
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 19:59:50 +0200

    Arduino: Setting body of item.live.h

--- arduino/src/http/live/item/item.live.h
+++ arduino/src/http/live/item/item.live.h
@@ -1,4 +1,9 @@
 #ifndef __ITEM_LIVE_H__
 #define __ITEM_LIVE_H__
 
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+
+void liveItem(uint8_t pin, const char* id, double idealConsumption,bool save);
+
 #endif
