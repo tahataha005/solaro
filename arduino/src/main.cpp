ed0db5aead2c8318a287533807525dd510f436e0
tahataha005 <taha.f.taha0@gmail.com>
2022-11-22 20:12:48 +0200

    Arduino: Setting up loop function in main.cpp to read and save data

--- arduino/src/main.cpp
+++ arduino/src/main.cpp
@@ -1,6 +1,11 @@
 #include <Arduino.h>
 #include "./wifi/wifi.h"
+#include "./http/live/item/item.live.h"
 
+//Setting itital settings
+const char* itemId = "6373547b22e7818efe58ac6d";
+double idealConsumption = 2;
+int counter = 0;
 
 //Main Setup function (Executed once)
 void setup() {
@@ -22,4 +27,20 @@ void setup() {
 //Main Loop function (Executed in a loop)
 void loop() {
 
+  //Loop delay
+  delay(100);
+
+  //Reading item and system statistics and display on screen
+  liveItem(D0,itemId,idealConsumption,false);
+
+
+  if (counter == 10) {
+    counter = 0;
+
+    //Saving item and system statistics to database every 1 second
+    liveItem(D0,itemId,idealConsumption,true);
+ 
+  }
+  
+  counter++;
 }
