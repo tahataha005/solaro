29b4fba6db50f9d5381b6c20837562fe1e79595e
tahataha005 <taha.f.taha0@gmail.com>
2022-11-23 12:31:37 +0200

    Arduino: Setting up body for request in controlItem in control.cpp

--- arduino/src/http/control/control.cpp
+++ arduino/src/http/control/control.cpp
@@ -1,6 +1,18 @@
 #include "control.h"
 
 void controlItem(uint8_t pin, const char* userId, const char* systemId, const char* itemId) {
+    //Initialising variables
+    HTTPClient http;
+    StaticJsonDocument<200> doc;
+    
+    //Setting body for request
+    doc["user_id"] = userId;
+    doc["system_id"] = systemId;
+    doc["item_id"] = itemId;
+
+    String json;
+    serializeJson(doc, json);
+
 
 }
 
