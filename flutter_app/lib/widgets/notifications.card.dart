480c35b8585a632997e34f1a4c7061d5a80fbb07
tahataha005 <taha.f.taha0@gmail.com>
2022-11-24 21:34:54 +0200

    Frontend: Adding main properties of notifications card

--- flutter_app/lib/widgets/notifications.card.dart
+++ flutter_app/lib/widgets/notifications.card.dart
@@ -1,4 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/tools/random.color.dart';
 
-class NotificationCard extends StatelessWidget {}
+class NotificationCard extends StatelessWidget {
+  final notification;
+
+  NotificationCard({required this.notification});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell();
+  }
+}
