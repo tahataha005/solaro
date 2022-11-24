034c0f2105984a8644bbbdf03485bfa99c1a5ec3
tahataha005 <taha.f.taha0@gmail.com>
2022-11-24 21:37:21 +0200

    Frontend: Implementing notification cards in notifications page

--- flutter_app/lib/pages/notifications.page.dart
+++ flutter_app/lib/pages/notifications.page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/providers/notifications.provider.dart';
+import 'package:flutter_app/widgets/notifications.card.dart';
 import 'package:provider/provider.dart';
 
 class NotificationsPage extends StatefulWidget {
@@ -19,75 +20,6 @@ class _NotificationsPageState extends State<NotificationsPage> {
       Colors.blueGrey,
     ];
     return colors[countName];
-  }
-
-  Widget notificationBuilder(notification) {
-    return InkWell(
-      onTap: () {},
-      splashColor: Theme.of(context).accentColor,
-      child: Container(
-        width: double.infinity,
-        decoration: BoxDecoration(
-          border: Border(
-            bottom: BorderSide(color: Color.fromARGB(255, 223, 223, 223)),
-          ),
-        ),
-        height: MediaQuery.of(context).size.height * 0.15,
-        child: Row(crossAxisAlignment: CrossAxisAlignment.start, children: [
-          Container(
-            margin: EdgeInsets.symmetric(
-                horizontal: 25,
-                vertical: MediaQuery.of(context).size.height * 0.035),
-            child: Center(
-              child: Text(
-                notification.system_name[0],
-                style: TextStyle(
-                  fontFamily: "Kanit",
-                  fontWeight: FontWeight.bold,
-                  fontSize: 50,
-                  color: Colors.white,
-                ),
-              ),
-            ),
-            height: MediaQuery.of(context).size.height * 0.08,
-            width: MediaQuery.of(context).size.height * 0.08,
-            decoration: BoxDecoration(
-              color: randColor(notification.system_name),
-              borderRadius: BorderRadius.all(
-                Radius.circular(10),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: MediaQuery.of(context).size.width * 0.45,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(vertical: 30),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    notification.system_name + ": " + notification.title,
-                    style: Theme.of(context).textTheme.titleMedium,
-                  ),
-                  Text(
-                    notification.content,
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(top: 15, left: 40),
-            child: Text(
-              notification.time,
-              style: Theme.of(context).textTheme.bodySmall,
-            ),
-          )
-        ]),
-      ),
-    );
   }
 
   @override
@@ -113,7 +45,7 @@ class _NotificationsPageState extends State<NotificationsPage> {
       ),
       body: Column(
         children: notifications.reversed.map((notification) {
-          return notificationBuilder(notification);
+          return NotificationCard(notification: notification);
         }).toList(),
       ),
     );
