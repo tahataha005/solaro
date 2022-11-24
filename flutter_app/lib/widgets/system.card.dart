007ddb26d5cd03d93a714dd6b180151a13f9a1b3
tahataha005 <taha.f.taha0@gmail.com>
2022-11-24 21:57:54 +0200

    Frontend: Structuring system card

--- flutter_app/lib/widgets/system.card.dart
+++ flutter_app/lib/widgets/system.card.dart
@@ -15,6 +15,60 @@ class SystemCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return InkWell();
+    return InkWell(
+      onTap: onTap,
+      splashColor: Theme.of(context).accentColor,
+      child: Container(
+        decoration: BoxDecoration(
+          border: Border(
+            bottom: BorderSide(
+              color: Color.fromARGB(255, 223, 223, 223),
+            ),
+          ),
+        ),
+        height: MediaQuery.of(context).size.height * 0.15,
+        child: Row(children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 25),
+            child: Center(
+              child: Text(
+                system.name[0],
+                style: TextStyle(
+                  fontFamily: "Kanit",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 50,
+                  color: Colors.white,
+                ),
+              ),
+            ),
+            height: MediaQuery.of(context).size.height * 0.08,
+            width: MediaQuery.of(context).size.height * 0.08,
+            decoration: BoxDecoration(
+                color: leadingColor,
+                borderRadius: BorderRadius.all(Radius.circular(10))),
+          ),
+          Flexible(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 30),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    system.name,
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Text(
+                    items,
+                    style: Theme.of(context).textTheme.bodySmall,
+                    overflow: TextOverflow.ellipsis,
+                  )
+                ],
+              ),
+            ),
+          ),
+        ]),
+      ),
+    );
   }
 }
