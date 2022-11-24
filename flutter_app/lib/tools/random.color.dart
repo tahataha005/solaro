2ff3cddd2adeb9d0898cb4a2862954e12ef91328
tahataha005 <taha.f.taha0@gmail.com>
2022-11-24 21:55:34 +0200

    Frontend: Returning color to passed string in random color

--- flutter_app/lib/tools/random.color.dart
+++ flutter_app/lib/tools/random.color.dart
@@ -1,3 +1,14 @@
 import 'package:flutter/material.dart';
 
-Color randColor(String name, context) {}
+Color randColor(String name, context) {
+  int countName = name.length;
+  while (countName >= 3) {
+    countName = countName % 3;
+  }
+  List colors = [
+    Theme.of(context).primaryColor,
+    Theme.of(context).accentColor,
+    Colors.blueGrey,
+  ];
+  return colors[countName];
+}
