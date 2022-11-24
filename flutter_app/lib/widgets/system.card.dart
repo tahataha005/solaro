d94f9d0befe7ceea076fdaa0b3fe7d6ca8f6cf0f
tahataha005 <taha.f.taha0@gmail.com>
2022-11-24 21:57:25 +0200

    Frontend: Adding properties of system card

--- flutter_app/lib/widgets/system.card.dart
+++ flutter_app/lib/widgets/system.card.dart
@@ -1,3 +1,20 @@
 import 'package:flutter/material.dart';
 
-class SystemCard extends StatelessWidget {}
+class SystemCard extends StatelessWidget {
+  final system;
+  final items;
+  final onTap;
+  final leadingColor;
+
+  SystemCard({
+    required this.system,
+    required this.items,
+    required this.leadingColor,
+    this.onTap = null,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell();
+  }
+}
