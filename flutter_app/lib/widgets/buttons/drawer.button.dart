099c9a1f64140b644e83b378c3bb2f739e798b72
tahataha005 <taha.f.taha0@gmail.com>
2022-11-25 09:16:08 +0200

    Frontend: Adding properties of drawer button

--- flutter_app/lib/widgets/buttons/drawer.button.dart
+++ flutter_app/lib/widgets/buttons/drawer.button.dart
@@ -1,3 +1,18 @@
 import 'package:flutter/material.dart';
 
-class DrawerButton extends StatelessWidget {}
+class DrawerButton extends StatelessWidget {
+  final String text;
+  bool selected;
+  final onPressed;
+
+  DrawerButton({
+    required this.text,
+    required this.selected,
+    this.onPressed = null,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox();
+  }
+}
