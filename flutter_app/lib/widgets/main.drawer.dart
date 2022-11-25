1c42154aa21d21a6641c30f823189314a59e0c7a
tahataha005 <taha.f.taha0@gmail.com>
2022-11-25 09:28:59 +0200

    Frontend: Constructing main drawer sections

--- flutter_app/lib/widgets/main.drawer.dart
+++ flutter_app/lib/widgets/main.drawer.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_app/providers/auth.provider.dart';
+import 'package:flutter_app/providers/items.provider.dart';
+import 'package:flutter_app/providers/notifications.provider.dart';
+import 'package:flutter_app/providers/systems.provider.dart';
+import 'package:flutter_app/widgets/buttons/drawer.button.dart';
+import 'package:provider/provider.dart';
 
 class MainDrawer extends StatefulWidget {
   @override
@@ -14,6 +20,45 @@ class _MainDrawerState extends State<MainDrawer> {
 
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Drawer(
+      child: Column(
+        children: [
+          Container(
+            height: MediaQuery.of(context).size.height * 0.4,
+            child: Column(
+              children: [
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  padding: EdgeInsets.only(top: 50),
+                  child: Image.asset("assets/images/Header-Logo.png"),
+                ),
+                DrawerButton(
+                  text: "Home",
+                  selected: selections["home"],
+                  onPressed: () {},
+                ),
+                DrawerButton(
+                  text: "Notifications",
+                  selected: selections["notifications"],
+                  onPressed: () {},
+                ),
+                DrawerButton(
+                  text: "Settings",
+                  selected: selections["settings"],
+                  onPressed: () {},
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: DrawerButton(
+              text: "Log Out",
+              selected: false,
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
