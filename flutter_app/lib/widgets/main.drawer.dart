feed47a09ec3c956a136019edecc8094020cd1b2
tahataha005 <taha.f.taha0@gmail.com>
2022-11-25 09:26:43 +0200

    Frontend: Creating main drawer in widgets

---
+++ flutter_app/lib/widgets/main.drawer.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class MainDrawer extends StatefulWidget {
+  @override
+  State<MainDrawer> createState() => _MainDrawerState();
+}
+
+class _MainDrawerState extends State<MainDrawer> {}
