39ebd679e27c8cc020ad470fd8bd610a401361fd
tahataha005 <taha.f.taha0@gmail.com>
2022-11-25 09:17:19 +0200

    Frontend: Styling drawer button

--- flutter_app/lib/widgets/buttons/drawer.button.dart
+++ flutter_app/lib/widgets/buttons/drawer.button.dart
@@ -17,9 +17,28 @@ class DrawerButton extends StatelessWidget {
       height: 50,
       width: MediaQuery.of(context).size.width * 0.7,
       child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: MaterialStateProperty.all(
+            selected
+                ? Theme.of(context).primaryColor
+                : Theme.of(context).primaryColorLight,
+          ),
+          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(15),
+            ),
+          ),
+        ),
         onPressed: onPressed,
         child: Text(
           text,
+          style: TextStyle(
+            color: selected
+                ? Theme.of(context).primaryColorLight
+                : Theme.of(context).primaryColor,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
         ),
       ),
     );
