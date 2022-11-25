151eb1949ee914320acdb4f94f25a463023f1ff2
tahataha005 <taha.f.taha0@gmail.com>
2022-11-25 09:29:40 +0200

    Frontend: Styling sections in main drawer

--- flutter_app/lib/widgets/main.drawer.dart
+++ flutter_app/lib/widgets/main.drawer.dart
@@ -22,10 +22,15 @@ class _MainDrawerState extends State<MainDrawer> {
   Widget build(BuildContext context) {
     return Drawer(
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Container(
+            padding: EdgeInsets.symmetric(horizontal: 20),
             height: MediaQuery.of(context).size.height * 0.4,
             child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              crossAxisAlignment: CrossAxisAlignment.center,
               children: [
                 Container(
                   width: MediaQuery.of(context).size.width * 0.5,
@@ -51,6 +56,7 @@ class _MainDrawerState extends State<MainDrawer> {
             ),
           ),
           Container(
+            padding: EdgeInsets.only(bottom: 50, left: 20, right: 20),
             child: DrawerButton(
               text: "Log Out",
               selected: false,
