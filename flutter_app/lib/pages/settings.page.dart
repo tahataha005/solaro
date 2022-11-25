166e1dcf02f0d9c5dad503705e30053686e9afca
tahataha005 <taha.f.taha0@gmail.com>
2022-11-25 09:35:36 +0200

    Frontend: Creating settings page

---
+++ flutter_app/lib/pages/settings.page.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatefulWidget {
+  @override
+  State<SettingsPage> createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {}
