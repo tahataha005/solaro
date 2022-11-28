3f394162036ea9853b76def738559b472d969184
tahataha005 <taha.f.taha0@gmail.com>
2022-11-29 00:14:49 +0200

    Frontend: Creating splash.screen.dart in widgets

---
+++ flutter_app/lib/widgets/splash.screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class CustomedSplashScreen extends StatefulWidget {
+  const CustomedSplashScreen({super.key});
+
+  @override
+  State<CustomedSplashScreen> createState() => Customed_SplashScreenState();
+}
+
+class Customed_SplashScreenState extends State<CustomedSplashScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
