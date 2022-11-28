8aa5ef055d177f061f5dc19f4a76728d683764d2
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:01:15 +0200

    Frontend: Adding properties of bright theme

--- flutter_app/lib/tools/themes/bright.theme.dart
+++ flutter_app/lib/tools/themes/bright.theme.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/tools/color.constructor.dart';
 
-class BrightTheme {}
+class BrightTheme {
+  ThemeData theme = ThemeData();
+
+  ThemeData get themeData => theme;
+}
