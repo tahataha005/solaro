2865257bb83eaacb053d8bd2bc04f3e8afe7a8c0
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:07:06 +0200

    Frontend: Setting bright title text themes in bright theme

--- flutter_app/lib/tools/themes/bright.theme.dart
+++ flutter_app/lib/tools/themes/bright.theme.dart
@@ -18,7 +18,27 @@ class BrightTheme {
     dividerColor: Color.fromARGB(255, 223, 223, 223),
     hintColor: Colors.white.withOpacity(0.6),
     fontFamily: "Inter",
-    textTheme: ThemeData.light().textTheme.copyWith(),
+    textTheme: ThemeData.light().textTheme.copyWith(
+          titleSmall: const TextStyle(
+            // drawer log in as
+            color: Color.fromRGBO(0, 114, 187, 1),
+            fontFamily: "kanit",
+            fontSize: 30,
+            fontWeight: FontWeight.bold,
+          ),
+          titleMedium: const TextStyle(
+            //card titles
+            fontFamily: "Inter",
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: const TextStyle(
+            //pages title
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Color.fromRGBO(255, 186, 73, 1),
+          ),
+        ),
   );
 
   ThemeData get themeData => theme;
