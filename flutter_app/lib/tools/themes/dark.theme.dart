67129c680c542d9f20696c2735d8e4ba233bdaf6
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:19:11 +0200

    Frontend: Setting dark structure colors in dark theme

--- flutter_app/lib/tools/themes/dark.theme.dart
+++ flutter_app/lib/tools/themes/dark.theme.dart
@@ -9,6 +9,16 @@ class DarkTheme {
         color: Colors.white,
       ),
     ),
+    primarySwatch: createMaterialColor(Color.fromARGB(255, 56, 129, 197)),
+    accentColor: Color.fromARGB(255, 255, 165, 20),
+    primaryColorLight: Color.fromARGB(255, 58, 58, 58),
+    scaffoldBackgroundColor: Color.fromARGB(255, 47, 47, 47),
+    backgroundColor: Color.fromARGB(255, 90, 90, 90),
+    cardColor: Color.fromARGB(255, 58, 58, 58),
+    dividerColor: Color.fromARGB(255, 76, 76, 76),
+    hintColor: Colors.black.withOpacity(0.4),
+    fontFamily: "Inter",
+    textTheme: ThemeData.light().textTheme.copyWith(),
   );
 
   ThemeData get themeData => theme;
