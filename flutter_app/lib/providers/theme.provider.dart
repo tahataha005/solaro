30a900fa6e107a58f93e310f0b37fa606e154adc
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:23:23 +0200

    Frontend: Adding properties of theme provider

--- flutter_app/lib/providers/theme.provider.dart
+++ flutter_app/lib/providers/theme.provider.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class Themes with ChangeNotifier {}
+class Themes with ChangeNotifier {
+  bool _darkMode = false;
+
+  bool get darkMode => _darkMode;
+}
