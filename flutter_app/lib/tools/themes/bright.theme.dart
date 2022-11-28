c9ac2d89ecc645e80c960952be138990cc99d421
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:04:14 +0200

    Frontend: Setting bright appBar theme in bright theme

--- flutter_app/lib/tools/themes/bright.theme.dart
+++ flutter_app/lib/tools/themes/bright.theme.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/tools/color.constructor.dart';
 
 class BrightTheme {
-  ThemeData theme = ThemeData();
+  ThemeData theme = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Colors.white,
+      iconTheme: IconThemeData(
+        color: Color.fromRGBO(0, 114, 187, 1),
+      ),
+    ),
+  );
 
   ThemeData get themeData => theme;
 }
