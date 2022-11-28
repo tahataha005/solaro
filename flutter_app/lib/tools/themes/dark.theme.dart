6b86a2b7c61012f4594d01131065f3aa28cc53b6
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:18:27 +0200

    Frontend: Setting dark appBar theme in dark theme

--- flutter_app/lib/tools/themes/dark.theme.dart
+++ flutter_app/lib/tools/themes/dark.theme.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter_app/tools/color.constructor.dart';
 
 class DarkTheme {
-  ThemeData theme = ThemeData();
+  ThemeData theme = ThemeData(
+    appBarTheme: AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 20, 20, 20),
+      iconTheme: IconThemeData(
+        color: Colors.white,
+      ),
+    ),
+  );
 
   ThemeData get themeData => theme;
 }
