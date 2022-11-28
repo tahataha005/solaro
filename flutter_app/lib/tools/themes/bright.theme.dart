35947ecbd2b20838bc31554ed8e83714f6c1217d
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:05:40 +0200

    Frontend: Setting bright structure colors in bright theme

--- flutter_app/lib/tools/themes/bright.theme.dart
+++ flutter_app/lib/tools/themes/bright.theme.dart
@@ -9,6 +9,16 @@ class BrightTheme {
         color: Color.fromRGBO(0, 114, 187, 1),
       ),
     ),
+    primarySwatch: createMaterialColor(Color.fromRGBO(0, 114, 187, 1)),
+    accentColor: Color.fromRGBO(255, 186, 73, 1),
+    primaryColorLight: Color.fromRGBO(233, 235, 248, 1),
+    scaffoldBackgroundColor: Colors.white,
+    backgroundColor: Colors.white,
+    cardColor: Color.fromRGBO(233, 235, 248, 1),
+    dividerColor: Color.fromARGB(255, 223, 223, 223),
+    hintColor: Colors.white.withOpacity(0.6),
+    fontFamily: "Inter",
+    textTheme: ThemeData.light().textTheme.copyWith(),
   );
 
   ThemeData get themeData => theme;
