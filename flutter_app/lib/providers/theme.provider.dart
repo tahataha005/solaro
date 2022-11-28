e17a12f763631f90483457e0af859d46bea763f9
tahataha005 <taha.f.taha0@gmail.com>
2022-11-28 14:23:48 +0200

    Frontend: Creating setDarkMode method in theme provider

--- flutter_app/lib/providers/theme.provider.dart
+++ flutter_app/lib/providers/theme.provider.dart
@@ -5,4 +5,11 @@ class Themes with ChangeNotifier {
   bool _darkMode = false;
 
   bool get darkMode => _darkMode;
+
+  void setDarkMode(bool value) async {
+    _darkMode = value;
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setBool('darkMode', value);
+    notifyListeners();
+  }
 }
