6fb8a37964104f315807cfc0c1dd303f73514153
tahataha005 <taha.f.taha0@gmail.com>
2022-11-29 08:35:45 +0200

    Backend: Authenticating arduino request in arduinoMiddleware

--- nodejs-server/middlewares/arduino.middleware.js
+++ nodejs-server/middlewares/arduino.middleware.js
@@ -1,3 +1,12 @@
-const arduinoMiddleware = (req, res, next) => {};
+const arduinoMiddleware = (req, res, next) => {
+    const { pass } = req.headers.authorization.pass;
+
+    //Check if the password is correct
+    if (!pass || pass !== process.env.ARDUINO_PASS) {
+        return res.status(401).json({ message: "Unauthorized" });
+    }
+
+    next();
+};
 
 module.exports = arduinoMiddleware;
